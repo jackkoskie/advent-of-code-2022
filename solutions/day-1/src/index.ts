ceb7790a1b46f429fc8e93a954f56af8d54cdefe
Jack Koskie <65452167+GoldenXLence@users.noreply.github.com>
2022-12-25 00:09:49 -0500

    chore: lint

--- solutions/day-1/src/index.ts
+++ solutions/day-1/src/index.ts
@@ -1,21 +1,21 @@
-import { readFileSync } from 'fs';
+import {readFileSync} from 'fs';
 
-const input: string[] = readFileSync('input.txt', 'utf8').split('\r\n\r\n')
+const input: string[] = readFileSync('input.txt', 'utf8').split('\r\n\r\n');
 
-const totals: number[] = []
+const totals: number[] = [];
 
-for (let i = 0; i < input.length; i++) {
-    const elfTotalString: string[] = input[i].split('\r\n')
-    const eflTotalNumber: number[] = elfTotalString.map((item) => parseInt(item))
-    const total: number = eflTotalNumber.reduce((a, b) => a + b)
-    totals.push(total)
+for (const elfNumbers of input) {
+	const elfTotalString: string[] = elfNumbers.split('\r\n');
+	const eflTotalNumber: number[] = elfTotalString.map(item => parseInt(item, 10));
+	const total: number = eflTotalNumber.reduce((a, b) => a + b);
+	totals.push(total);
 }
 
-const highest: number = Math.max(...totals)
-const secondHighest: number = Math.max(...totals.filter((item) => item !== highest))
-const thirdHighest: number = Math.max(...totals.filter((item) => item !== highest && item !== secondHighest))
+const highest: number = Math.max(...totals);
+const secondHighest: number = Math.max(...totals.filter(item => item !== highest));
+const thirdHighest: number = Math.max(...totals.filter(item => item !== highest && item !== secondHighest));
 
-console.log(`Elf #${totals.indexOf(highest) + 1} has the highest total of ${highest}`)
-console.log(`Elf #${totals.indexOf(secondHighest) + 1} has the second highest total of ${secondHighest}`)
-console.log(`Elf #${totals.indexOf(thirdHighest) + 1} has the third highest total of ${thirdHighest}`)
-console.log(`The top three elves have a total of ${highest + secondHighest + thirdHighest}`)
+console.log(`Elf #${totals.indexOf(highest) + 1} has the highest total of ${highest}`);
+console.log(`Elf #${totals.indexOf(secondHighest) + 1} has the second highest total of ${secondHighest}`);
+console.log(`Elf #${totals.indexOf(thirdHighest) + 1} has the third highest total of ${thirdHighest}`);
+console.log(`The top three elves have a total of ${highest + secondHighest + thirdHighest}`);
